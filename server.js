41301befae3c53d789ddc3bb7fd4eda57799dbb4
amoslgl96 <baolgl96@gmail.com>
2019-02-14 23:21:53 +0800

    created json api routes for posts, profile and users

--- server.js
+++ server.js
@@ -2,15 +2,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-
 const app = express();
 
+// retrieve routes objects
+const posts = require("./routes/api/posts");
+const profiles = require("./routes/api/profile");
+const users = require("./routes/api/users");
+
+//retrieve db object
+const db = require("./config/keys").mongoURI;
+
 app.get("/", (req,res) => res.send("hello"));
+
+//set up route listeners
+app.use("/api/posts", posts);
+app.use("/api/profile", profiles);
+app.use("/api/users", users);
 
 
 const port = process.env.PORT || 5000;
 
-const db = require("./config/keys").mongoURI;
 
 mongoose
 .connect(db,{useNewUrlParser: true}).
