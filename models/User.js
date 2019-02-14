a93046ef2db6f1c7f743b6ec7678ec7803b0f34f
amoslgl96 <baolgl96@gmail.com>
2019-02-14 23:44:49 +0800

    created  user schema with mongoose

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+//Create Schema
+
+//avatar: when they register with their email, we will query the avatar server to retrieve
+//the avatar. If no avatar, a placeholder image will be the substitute.
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    avatar: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+
+module.exports = mongoose.model("users",UserSchema);
